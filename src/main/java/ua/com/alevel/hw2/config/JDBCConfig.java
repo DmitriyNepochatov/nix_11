a071a09034788202d6948f5f89929dedb7085b35
DmitriyNepochatov <dima2001nepochatov@gmail.com>
2022-09-01 14:21:18 +0300

    Heroku7

--- src/main/java/ua/com/alevel/hw2/config/JDBCConfig.java
+++ src/main/java/ua/com/alevel/hw2/config/JDBCConfig.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class JDBCConfig {
-    private static final String URL = "jdbc:postgresql://khwmscwlrzvnjl:3da62380e3c79bbcac5ca9dc0f5936b03b8ebf238a8507516b39a5886ea17725@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/dfvea792kece9j";
+    private static final String URL = "jdbc:postgresql://ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/dfvea792kece9j";
     private static final String USER = "khwmscwlrzvnjl";
     private static final String PASSWORD = "3da62380e3c79bbcac5ca9dc0f5936b03b8ebf238a8507516b39a5886ea17725";
 
