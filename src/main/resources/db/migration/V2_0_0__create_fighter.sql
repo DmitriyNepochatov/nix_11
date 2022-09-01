83a84e6546f43764b8c9d8911d6ec16785f52ae1
DmitriyNepochatov <dima2001nepochatov@gmail.com>
2022-09-01 17:04:44 +0300

    Heroku9

--- src/main/resources/db/migration/V2_0_0__create_fighter.sql
+++ src/main/resources/db/migration/V2_0_0__create_fighter.sql
@@ -1,9 +1,9 @@
-INSERT INTO public.fighter(
+INSERT INTO "public"."fighter"(
     id, brand, count, created, currency, model, price, invoice_id, bomb_load, type_of_fighter)
 VALUES ('63d42d23-c031-4157-9d59-88afc1f1f2c7', 4, 53, '2022-08-31 20:15:24.036', 'C34', 'Model-171', 864, null, 6, 4);
-INSERT INTO public.fighter(
+INSERT INTO "public"."fighter"(
     id, brand, count, created, currency, model, price, invoice_id, bomb_load, type_of_fighter)
 VALUES ('f1e2c46a-5f36-40f5-a4b1-0c713772a844', 2, 35, '2022-08-31 20:15:24.036', 'C52', 'Model-160', 1637, null, 32, 3);
-INSERT INTO public.fighter(
+INSERT INTO "public"."fighter"(
     id, brand, count, created, currency, model, price, invoice_id, bomb_load, type_of_fighter)
 VALUES ('e7520e43-db8c-4126-9fb1-a66d0a1f2112', 4, 137, '2022-08-31 20:15:24.036', 'C80', 'Model-1', 2392, null, 21, 2);
