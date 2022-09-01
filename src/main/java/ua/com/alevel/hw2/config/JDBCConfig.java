f416d1e38d80bf7df24470d9b9a51947fdc141d7
DmitriyNepochatov <dima2001nepochatov@gmail.com>
2022-09-01 13:27:49 +0300

    Heroku2

--- src/main/java/ua/com/alevel/hw2/config/JDBCConfig.java
+++ src/main/java/ua/com/alevel/hw2/config/JDBCConfig.java
@@ -5,9 +5,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public final class JDBCConfig {
-    private static final String URL = "jdbc:postgresql://localhost:5432/PlaneShop";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "0000";
+    private static final String URL = "postgres://khwmscwlrzvnjl:3da62380e3c79bbcac5ca9dc0f5936b03b8ebf238a8507516b39a5886ea17725@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/dfvea792kece9j";
+    private static final String USER = "khwmscwlrzvnjl";
+    private static final String PASSWORD = "3da62380e3c79bbcac5ca9dc0f5936b03b8ebf238a8507516b39a5886ea17725";
 
     private JDBCConfig() {
     }
